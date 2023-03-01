ebce73b7c3daccc00072ddf5e8ee5a6cd29364f4
Juan Fernandes <113736913+juanjpf@users.noreply.github.com>
2023-02-28 22:50:49 -0300

    script.sql

---
+++ SQL/script.sql
@@ -0,0 +1,104 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema diagrama
+-- -----------------------------------------------------
+
+CREATE SCHEMA IF NOT EXISTS `diagrama` DEFAULT CHARACTER SET utf8 ;
+USE `diagrama` ;
+
+-- -----------------------------------------------------
+-- Table `diagrama`.`Cursos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `diagrama`.`Cursos` (
+  `idCursos` INT NOT NULL AUTO_INCREMENT,
+  `Nome_Curso` VARCHAR(45) NULL,
+  `Turno` VARCHAR(255) NULL,
+  `Inicio` int,
+  `Termino` int,
+  PRIMARY KEY (`idCursos`))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `diagrama`.`Turmas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `diagrama`.`Turmas` (
+  `idTurmas` INT NOT NULL AUTO_INCREMENT,
+  `Facilitadores` VARCHAR(255) NULL,
+  `Turno` VARCHAR(255) NULL,
+  `Alunos` VARCHAR(255) NULL,
+  PRIMARY KEY (`idTurmas`),
+  CONSTRAINT `fk_Turmas_Cursos`
+    FOREIGN KEY (`idTurmas`)
+    REFERENCES `diagrama`.`Cursos` (`idCursos`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `diagrama`.`Matricula`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `diagrama`.`Matricula` (
+  `idMatricula` INT NOT NULL AUTO_INCREMENT,
+  `Endereco` VARCHAR(255) NULL,
+  `Curso` VARCHAR(255) NULL,
+  PRIMARY KEY (`idMatricula`))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `diagrama`.`Alunos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `diagrama`.`Alunos` (
+  `idAlunos` INT NOT NULL AUTO_INCREMENT,
+  `Nome` VARCHAR(255) NULL,
+  `Idade` INT,
+  `Endereco` VARCHAR(255),
+  PRIMARY KEY (`idAlunos`),
+  CONSTRAINT `fk_Alunos_Turmas1`
+    FOREIGN KEY (`idAlunos`)
+    REFERENCES `diagrama`.`Turmas` (`idTurmas`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `diagrama`.`Facilitadores`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `diagrama`.`Facilitadores` (
+  `idFacilitadores` INT NOT NULL AUTO_INCREMENT,
+  `Nome` VARCHAR(255) NULL,
+  `Curso` VARCHAR(255) NULL,
+  PRIMARY KEY (`idFacilitadores`, `Curso`),
+  INDEX `fk_Facilitadores_Turmas1_idx` (`Curso` ASC) VISIBLE,
+  CONSTRAINT `fk_Facilitadores_Turmas1`
+    FOREIGN KEY (`Curso`)
+    REFERENCES `diagrama`.`Turmas` (`idTurmas`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `diagrama`.`Endereco`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `diagrama`.`Endereco` (
+  `idEndereco` INT NOT NULL AUTO_INCREMENT,
+  `Rua` VARCHAR(255) NULL,
+  `Numero` VARCHAR(255) NULL,
+  `Bairro` VARCHAR(255) NULL,
+  `Estado` VARCHAR(255) NULL,
+  `Telefone` VARCHAR(255) NULL,
+  PRIMARY KEY (`idEndereco`),
+  CONSTRAINT `fk_Endereco_Matricula1`
+    FOREIGN KEY (`idEndereco`)
+    REFERENCES `diagrama`.`Matricula` (`endereco`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
